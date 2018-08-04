1c4694a83f6154448d54042ca4b1470b4eb588bf
Vitalii Danylov <v.danylov@ideamotive.co>
2018-08-04 23:43:12 +0200

    Added material widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 main() {}
 
-class MyCoolApp extends StatelessWidget {}
+class MyCoolApp extends StatelessWidget {
+  build(context) {
+    return MaterialApp();
+  }
+}
